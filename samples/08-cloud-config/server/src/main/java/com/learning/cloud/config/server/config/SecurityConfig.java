f8eaf08570c2ef1ebdcd4a9c9afc21693ad1bf4b
Luciano Sampaio <lsampaioweb@gmail.com>
2024-06-29 13:46:13 -0300

    refactor: Add the code to restrict access to certain users.

--- samples/08-cloud-config/server/src/main/java/com/learning/cloud/config/server/config/SecurityConfig.java
+++ samples/08-cloud-config/server/src/main/java/com/learning/cloud/config/server/config/SecurityConfig.java
@@ -10,34 +10,35 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
+@Configuration
 public class SecurityConfig {
 
-  // @Bean
-  // @Order(1)
-  // SecurityFilterChain client1SecurityFilterChain(HttpSecurity http) throws Exception {
-  //   http
-  //       .securityMatcher("/cloud-config-client/**")
-  //       .authorizeHttpRequests(
-  //           authorize -> authorize.anyRequest()
-  //               .hasRole("cloud-config-client"))
-  //       .httpBasic(Customizer.withDefaults());
+  @Bean
+  @Order(1)
+  SecurityFilterChain client1SecurityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .securityMatcher("/cloud-config-client/**")
+        .authorizeHttpRequests(
+            authorize -> authorize.anyRequest()
+                .hasRole("cloud-config-client"))
+        .httpBasic(Customizer.withDefaults());
 
-  //   return http.build();
-  // }
+    return http.build();
+  }
 
-  // @Bean
-  // UserDetailsService userDetailsService() {
-  //   InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+  @Bean
+  UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
-  //   manager.createUser(User
-  //       .withUsername("cloud-config-client")
-  //       .password("{noop}password")
-  //       .roles("cloud-config-client")
-  //       .build());
+    manager.createUser(
+        User
+            .withUsername("cloud-config-client")
+            .password("{noop}password")
+            .roles("cloud-config-client")
+            .build());
 
-  //   // manager.createUser(User.withUsername("client2").password("{noop}client2password").roles("CLIENT2").build());
+    // manager.createUser(User.withUsername("client2").password("{noop}client2password").roles("CLIENT2").build());
 
-  //   return manager;
-  // }
+    return manager;
+  }
 }
