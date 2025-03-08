da85daacf961a231d64bbcd984827839b1570a23
Luciano Sampaio <lsampaioweb@gmail.com>
2025-03-08 17:22:23 -0300

    feat: Add Traefik sample with Docker configuration and logging setup

---
+++ samples/17-traefik/src/main/java/com/learning/traefik/TraefikApplication.java
@@ -0,0 +1,13 @@
+package com.learning.traefik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraefikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraefikApplication.class, args);
+	}
+
+}
