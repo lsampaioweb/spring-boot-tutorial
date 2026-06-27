999d16793d23c336acc5ee67a4be2bb0a8db7dcc
Luciano Sampaio <lsampaioweb@gmail.com>
2026-06-27 18:28:27 -0300

    feat(container): i18n-based host info logging.

--- samples/16-container/src/main/java/com/learning/container/user/UserController.java
+++ samples/16-container/src/main/java/com/learning/container/user/UserController.java
@@ -2,6 +2,8 @@ package com.learning.container.user;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learning.container.i18n.LogMessages;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
@@ -11,19 +13,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 @Slf4j
 public class UserController {
+
+  private static final String LOG_HOST_INFO = "log.container.host.info";
+
+  private final LogMessages logMessages;
+
+  UserController(LogMessages logMessages) {
+    this.logMessages = logMessages;
+  }
 
   @GetMapping("/hello")
   public String sayHello() throws UnknownHostException {
     String hostName = InetAddress.getLocalHost().getHostName();
     String hostAddress = InetAddress.getLocalHost().getHostAddress();
 
-    String message = String.format("Hostname: %s and IP Address: %s", hostName, hostAddress);
+    log.info(logMessages.get(LOG_HOST_INFO, hostName, hostAddress));
 
-    log.info(message);
-
-    return message;
+    return String.format("Hostname: %s and IP Address: %s", hostName, hostAddress);
   }
 }
